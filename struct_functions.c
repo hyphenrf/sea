7c477541e9e7e190830c7fd78a4f7647d4959f1f
Haz <hyphens@pm.me>
2020-09-27 06:35:40 +0200

    Imagining how C++ implemented classes

--- struct_functions.c
+++ struct_functions.c
@@ -5,6 +5,7 @@
 int meaning(int a) {
 		return 42*a / a;
 }
+void nothing_burger(void){};
 
 struct hummus {
 	int id;
@@ -13,12 +14,17 @@ struct hummus {
 //const struct hummus DEFAULT_HUMMUS = {0, &meaning}
 } DEFAULT_HUMMUS = {0, &meaning};
 
+typedef struct{void (*nothing_burger)(void);} nothing;
+// `nothing` is sort-of class now
+
 int main(int argc, char* argv[])
 {
 	struct hummus s = DEFAULT_HUMMUS;
+	nothing news;
 	int result;
 	
 	result = s.meaninig(5);
 	printf("%d\n", result);
+	news.nothing_burger();
 	return 0;
 }
