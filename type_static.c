3b68fa6499c1107fbaeff00a1b8fa22d1b7ddef5
Haz <hyphens@pm.me>
2020-11-19 23:15:26 +0200

    more info on static qualifiers

--- type_static.c
+++ type_static.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 /* 
- * Static fields in C retain their value across the program's runtime. We can
- * use them the same way we'd use python generators for example:
+ * Static fields in C retain their value across the program's runtime.
+ *
+ * In process structure: static vars, like globals, are stored in the .data
+ * section.. However, unlike globals, their scope is limited to where they were
+ * declared.
+ * Remember that normal scoped vars are stored in registers and/or on stack/heap.
+ *
+ * We can use them the same way we'd use python's generators. Creating an opaque
+ * function interface. for example:
  */
 	
 int
 num_generator(void)
 {
-	static int arthur = -1;
-	           arthur++;
-	return arthur;
+	static int arthur = 0; //scoped and inaccessible, defined only once
+	                       //(despide being "declared" on every call)
+	return arthur++;
 }
 
 int main(int argc, char* argv[])
@@ -24,3 +31,11 @@ int main(int argc, char* argv[])
 	
 	return 0;
 }
+
+/* The static functions are a much simpler concept: A static function is one
+ * which has its scope limited to the file it's declared in. That allows reuse
+ * of function names across a project */
+
+static int hello(void) { return 5; }
+
+/* Try combining extern and static; the compiler will yell at you. */
