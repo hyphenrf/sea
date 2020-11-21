6ae4f5ab1012109e732b8f38d91d52fa9a4b667b
Haz <hyphens@pm.me>
2020-11-21 22:18:04 +0200

    emphasize that static vars are auto-init to 0

--- type_static.c
+++ type_static.c
@@ -14,8 +14,13 @@
 int
 num_generator(void)
 {
-	static int arthur = 0; //scoped and inaccessible, defined only once
-	                       //(despide being "declared" on every call)
+	/* static local variables and global variables without the static keyword, are
+ 	 * initialized to either zero, or their defined value. */
+
+	static int arthur; //scoped and inaccessible, initialized to 0 by default
+	/* Note: even if you manually init this to 0, it will only be defined once and
+ 	 * its value will change on each subsequent call without being reset to 0 on
+ 	 * each call. */
 	return arthur++;
 }
 
